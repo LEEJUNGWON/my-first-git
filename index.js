24bc675cd891797d1d5267160e959c65ebea2965
otherUser <starcsiran0@naver.com>
2016-12-12 14:59:46 +0900

    스크립트 변경

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var btn = document.getElementById("login");
 var reg = document.getElementById("regist");
 
-btn.addEventListener("click", function(){
-   alert("로그인 버튼을 클릭하였습니다."); 
+btn.addEventListener("click", function(){   
+   var userId = document.getElementById("userId").value;
+   var userPw = document.getElementById("userPw").value;
+    if(userId !=null && userPw !=null){
+        alert("로그인에 성공하였습니다.");
+    }else{
+        alert("빈 값을  채워 주세요.");
+    }
 });
 
 reg.addEventListener("click", function(){
